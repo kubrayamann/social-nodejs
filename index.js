3a270cde1e9a735ceef1a767a6c7b54836a1c0a2
yamankubra <ymn.kubra@gmail.com>
2022-05-02 00:39:27 +0300

    created posts page

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
-const autRoute = require("./routes/auth");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
  
@@ -27,7 +28,8 @@ app.use(morgan("common"));
 
 
 app.use("/api/users", userRoute);
-app.use("/api/auth", autRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
  
 app.listen(8800, () => {
   console.log("Backend Server is running on port 8800");
