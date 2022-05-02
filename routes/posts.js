ccde262cc19e4fbffe79391f7739fc915fb9f1b7
yamankubra <ymn.kubra@gmail.com>
2022-05-03 00:39:01 +0300

    created post

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const Post = require("../models/Post");
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+//CREATE A NEW POST
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try{
+        const savedPost = await newPost.save();
+        return res.status(200).json(savedPost);
+    }catch(err){
+        return res.status(500).json(err);
+    }
 });
+
+//UPDATE A POST
+//DELETE A POST
+//LIKE A POST
+//GET A POST
+//GET A TIME LINE OF POSTS
 
 module.exports = router;
