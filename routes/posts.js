64b7de7df4d8fc2ccec848bd51b4b76cd3679065
yamankubra <ymn.kubra@gmail.com>
2022-05-03 00:50:59 +0300

    updated post

--- routes/posts.js
+++ routes/posts.js
@@ -13,6 +13,23 @@ router.post("/", async (req, res) => {
 });
 
 //UPDATE A POST
+
+router.put("/:id", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await post.updateOne({
+                $set : req.body
+            });
+            return res.status(200).json("The post has been updated successfully");
+        }else{
+            return res.status(403).json("You can updated only your posts");
+        }
+    }catch(err){
+        return res.status(500).json(err);
+    }
+});
+
 //DELETE A POST
 //LIKE A POST
 //GET A POST
