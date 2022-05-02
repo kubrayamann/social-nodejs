e25c386cec861222c714cb0f2f5f8b1e2bace431
yamankubra <ymn.kubra@gmail.com>
2022-05-03 01:38:43 +0300

    get timeline post

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
+
 
 //CREATE A NEW POST
 router.post("/", async (req, res) => {
@@ -72,6 +74,31 @@ router.put("/:id/like", async (req, res) => {
     }
 });
 //GET A POST
+
+router.get("/:id", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        return res.status(200).json(post);
+    }catch(err){
+        return res.status(500).json(err);
+    }
+});
+
 //GET A TIME LINE OF POSTS
+
+router.get("/timeline/all", async (req, res) => { 
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId:currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.followings.map(friendId => {
+                return Post.find({userId : friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts));
+    }catch(err){
+        return res.status(500).json(err);
+    }
+});
 
 module.exports = router;
